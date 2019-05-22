e2c9efaf262f79330683cd0d7196995015270506
Steven Fisher <steven.fisher22@gmail.com>
2019-05-22 15:40:38 -0500

    stay working

--- JS/blackjack.js
+++ JS/blackjack.js
@@ -18,17 +18,9 @@ document.getElementById("btn-stay").addEventListener("click", stay)
 
 
 
-//////////
-// DEAL //
-//////////
-
-// When 'Deal' button is clicked, deal 2 cards each to: 
-// id="dealer-hand" and id="player-hand"
-// To-Do:
-// 1. Take away a card from the deck array
-// 2. Save the dealt card in two arrays: dealerCards and playerCards
-// 3. The dealerCards and playerCards arrays will be used later to calculate total points for the hand
-// 4. Display the img of the card on the page
+///////////////////////////////////////////////////
+// SETUP BASICS - PLAYER/DEALER HANDS AND POINTS //
+///////////////////////////////////////////////////
 
 var dealerCards = []
 var playerCards = []
@@ -41,6 +33,15 @@ var playerScore = document.getElementById('player-points')
 
 var playerPoints = 0
 var dealerPoints = 0
+
+
+
+//////////
+// DEAL //
+//////////
+
+// When 'Deal' button is clicked, deal 2 cards each to: 
+// id="dealer-hand" and id="player-hand"
 
 function deal(){
     var card1 = deck.pop();
@@ -84,14 +85,14 @@ function deal(){
 // id="player-hand"
 
 function hit(){
-    dealACard();
+    hitACard();
     if (playerPoints > 21) {
         $('#messages').text('Sorry, you busted!');
         gameOver();
     }
 }
 
-function dealACard() {
+function hitACard() {
     var cardHit = deck.pop();
     var createImgHit = document.createElement('img');
     createImgHit.setAttribute('src', cardHit.cardImg);
@@ -110,11 +111,32 @@ function dealACard() {
 // Deal cards to dealer until he reaches 17 points or more
 
 function stay(){
+    // Dealer 'hits' as long as dealer points are under 17
+    while (dealerPoints < 17) {
+        stayACard();
+    }
+    // Check for bust
+    if (dealerPoints > 21) {
+        $('#messages').text('YAY, DEALER BUSTED AND YOU WON!');
+    } else {
+        if (playerPoints > dealerPoints) {
+            $('#messages').text('YOU WON!')
+        } else if (dealerPoints > playerPoints) {
+            $('#messages').text('SORRY, YOU LOST!')
+        } else {
+            $('#messages').text('PUSH.')
+        }
+    }
+    gameOver();
+}
+
+function stayACard() {
     var cardStay = deck.pop();
     var createImgStay = document.createElement('img')
     createImgStay.setAttribute('src', cardStay.cardImg)
     dealerHand.append(createImgStay)
     dealerPoints += cardStay.points;
+    dealerScore.textContent = dealerPoints;
 }
 
 
